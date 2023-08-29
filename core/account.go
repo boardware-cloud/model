de5468603c8393c35dcde6c1d0a91f12bfff6ccd
Dan Chen <dan.chen@boardware.com>
2023-08-29 10:01:35 +0800

    feat

--- core/account.go
+++ core/account.go
@@ -9,12 +9,13 @@ import (
 
 type Account struct {
 	gorm.Model
-	ID       uint           `gorm:"primarykey"`
-	Email    string         `json:"email" gorm:"index:email_index,unique"`
-	Password string         `json:"password" gorm:"type:CHAR(128)"`
-	Salt     []byte         `json:"salt"`
-	Role     constants.Role `json:"role" gorm:"type:VARCHAR(128)"`
-	Totp     *string
+	ID        uint           `gorm:"primarykey"`
+	Email     string         `json:"email" gorm:"index:email_index,unique"`
+	Password  string         `json:"password" gorm:"type:CHAR(128)"`
+	Salt      []byte         `json:"salt"`
+	Role      constants.Role `json:"role" gorm:"type:VARCHAR(128)"`
+	Enable2FA bool
+	Totp      *string
 }
 
 func (a *Account) BeforeCreate(tx *gorm.DB) (err error) {
