d16ac24b3e46651b9c8ccdb19177354f3b0ecda0
Dan Chen <dan.chen@boardware.com>
2023-08-30 10:34:52 +0800

    feat

---
+++ core/ticket.go
@@ -0,0 +1,18 @@
+package core
+
+import (
+	"github.com/boardware-cloud/common/utils"
+	"gorm.io/gorm"
+)
+
+type Ticket struct {
+	gorm.Model
+	AccountId uint `json:"accountId" gorm:"index:account_id_index"`
+	Type      string
+	Secret    string
+}
+
+func (t *Ticket) BeforeCreate(tx *gorm.DB) (err error) {
+	t.ID = utils.GenerteId()
+	return
+}
