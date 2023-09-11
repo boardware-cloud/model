4fd98040d4769d8c3ad7178f6d12ed4ce576dccb
Dan Chen <dan.chen@boardware.com>
2023-09-11 14:12:28 +0800

    feat

--- core/init.go
+++ core/init.go
@@ -11,7 +11,6 @@ func Init(injectDB *gorm.DB) {
 	db = injectDB
 	db.AutoMigrate(&Account{})
 	db.AutoMigrate(&Credential{})
-	db.AutoMigrate(&LoginRecord{})
 	db.AutoMigrate(&SessionData{})
 	db.AutoMigrate(Session{})
 	db.AutoMigrate(&Ticket{})
--- core/session.go
+++ core/session.go
@@ -22,13 +22,3 @@ func (a *Session) BeforeCreate(tx *gorm.DB) (err error) {
 	a.ID = utils.GenerteId()
 	return
 }
-
-type LoginRecord struct {
-	gorm.Model
-	AccountId uint `json:"accountId" gorm:"index:accountId_index"`
-}
-
-func (a *LoginRecord) BeforeCreate(tx *gorm.DB) (err error) {
-	a.ID = utils.GenerteId()
-	return
-}
