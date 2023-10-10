a86cefc9f7c143cf79d6776109c04cbad2e63fba
Dan Chen <dan.chen@boardware.com>
2023-10-10 12:31:43 +0800

    feat

--- argus/node.go
+++ argus/node.go
@@ -1,9 +1,17 @@
 package argus
 
-import "gorm.io/gorm"
+import (
+	"github.com/boardware-cloud/common/utils"
+	"gorm.io/gorm"
+)
 
 type ArgusNode struct {
 	gorm.Model
 	Heartbeat         int64
 	HeartbeatInterval int64
+}
+
+func (a *ArgusNode) BeforeCreate(tx *gorm.DB) (err error) {
+	a.ID = utils.GenerteId()
+	return
 }
