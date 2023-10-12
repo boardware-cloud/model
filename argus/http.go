7de9243dc656ed15afa011afc2c0e55bf6e27427
Dan Chen <dan.chen@boardware.com>
2023-10-12 14:52:54 +0800

    feat

--- argus/http.go
+++ argus/http.go
@@ -6,15 +6,18 @@ import (
 	"time"
 
 	"github.com/boardware-cloud/common/constants"
+	"github.com/boardware-cloud/model/common"
 )
 
 type HttpMonitor struct {
-	Type       string               `json:"type"`
-	Interval   time.Duration        `json:"interval"`
-	Timeout    int64                `json:"timeout"`
-	Url        string               `json:"url"`
-	Retries    int64                `json:"retries"`
-	HttpMethod constants.HttpMehotd `json:"method" gorm:"type:VARCHAR(128)"`
+	Type                string               `json:"type"`
+	Interval            time.Duration        `json:"interval"`
+	Timeout             int64                `json:"timeout"`
+	Url                 string               `json:"url"`
+	Retries             int64                `json:"retries"`
+	HttpMethod          constants.HttpMehotd `json:"method" gorm:"type:VARCHAR(128)"`
+	Headers             common.PairList      `json:"headers"`
+	AcceptedStatusCodes common.StringList    `json:"acceptedStatusCodes"`
 }
 
 func (h HttpMonitor) ToJSON() MonitorJSON {
