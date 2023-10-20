cec20e64467bff8a8eb61427ea3449093b9f0806
Dan Chen <dan.chen@boardware.com>
2023-10-20 11:03:23 +0800

    feat

--- core/ticket.go
+++ core/ticket.go
@@ -1,6 +1,9 @@
 package core
 
 import (
+	"math/rand"
+	"time"
+
 	"github.com/boardware-cloud/common/utils"
 	"gorm.io/gorm"
 )
@@ -23,4 +26,29 @@ func NewTicketRepository(db *gorm.DB) TicketRepository {
 
 type TicketRepository struct {
 	db *gorm.DB
+}
+
+func (t TicketRepository) CreateTicket(typ string, accountId uint) Ticket {
+	var ticket Ticket
+	ticket.AccountId = accountId
+	ticket.Secret = RandomNumberString(16)
+	ticket.Type = typ
+	t.db.Save(&ticket)
+	return ticket
+}
+
+const charset = "0123456789"
+
+var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func RandomNumberString(length int) string {
+	return StringWithCharset(length, charset)
+}
+
+func StringWithCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
 }
