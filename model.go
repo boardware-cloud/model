e6a2aa3e5ca20bcf211b4c0ecf18247116cd0ad2
Dan Chen <dan.chen@boardware.com>
2023-11-20 09:56:46 +0800

    feat

--- model.go
+++ model.go
@@ -7,16 +7,11 @@ import (
 	"gorm.io/gorm"
 )
 
-var db *gorm.DB
-
-func NewConnection(user, password, host, port, database string) (*gorm.DB, error) {
+func NewConnection(user, password, host, port, database string) (db *gorm.DB, err error) {
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		user, password, host, port, database,
 	)
-	var err error
-	if db == nil {
-		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	}
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	return db, err
 }
