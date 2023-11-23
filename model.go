4bff54a4798ce399a8bd354a116cbe56363f82f2
Dan Chen <dan.chen@boardware.com>
2023-11-23 14:44:01 +0800

    feat

--- model.go
+++ model.go
@@ -13,7 +13,12 @@ var db *gorm.DB
 func GetDB() *gorm.DB {
 	if db == nil {
 		var err error
-		db, err = NewConnection(config.GetString("db.user"), config.GetString("db.password"), config.GetString("db.host"), config.GetString("db.port"), config.GetString("db.database"))
+		db, err = NewConnection(
+			config.GetString("database.user"),
+			config.GetString("database.password"),
+			config.GetString("database.host"),
+			config.GetString("database.port"),
+			config.GetString("database.database"))
 		if err != nil {
 			panic(err)
 		}
@@ -21,11 +26,11 @@ func GetDB() *gorm.DB {
 	return db
 }
 
-func NewConnection(user, password, host, port, database string) (db *gorm.DB, err error) {
+func NewConnection(user, password, host, port, database string) (*gorm.DB, error) {
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		user, password, host, port, database,
 	)
-	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	return db, err
 }
