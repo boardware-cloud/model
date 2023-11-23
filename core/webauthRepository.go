44a1f0f4ad08d58f9f330e65b8063bae2846a5d5
Dan Chen <dan.chen@boardware.com>
2023-11-23 10:08:18 +0800

    feat

--- core/webauthRepository.go
+++ core/webauthRepository.go
@@ -27,3 +27,33 @@ func (w WebauthRepository) List(conds ...any) []Credential {
 	w.db.Find(&credentials, conds...)
 	return credentials
 }
+
+var sessionDataRepository *SessionDataRepository
+
+func GetSessionDataRepository() *SessionDataRepository {
+	if sessionDataRepository == nil {
+		sessionDataRepository = NewSessionDataRepository()
+	}
+	return sessionDataRepository
+}
+
+func NewSessionDataRepository() *SessionDataRepository {
+	return &SessionDataRepository{model.GetDB()}
+}
+
+type SessionDataRepository struct {
+	db *gorm.DB
+}
+
+func (s SessionDataRepository) Find(conds ...any) *SessionData {
+	var sessionData SessionData
+	ctx := s.db.Find(&sessionData, conds...)
+	if ctx.RowsAffected == 0 {
+		return nil
+	}
+	return &sessionData
+}
+
+func (s SessionDataRepository) GetById(id uint) *SessionData {
+	return s.Find(id)
+}
